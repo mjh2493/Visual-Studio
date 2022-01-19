1ac89db89a88761dd003c902dcb5ed91d563294f
mjh2493 <mjh2493@gmail.com>
2022-01-18 20:38:06 -0500

    Visual Studio HTML Assignment

---
+++ MusicVideo/MusicVideo.Website/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function openVideo() {
+    document.getElementById("myVideo").style.display = "block";
+}
+
+function closeVideo() {
+    document.getElementById("myVideo").style.display = "none";
+}
